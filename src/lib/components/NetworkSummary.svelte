65721152e1629a0c6fca4473f43d1d473c99d637
Gabriel Chartier <gabriel@chartier.link>
2024-12-17 11:54:46 -0500

    update network summary with nodes and sources data

--- src/lib/components/NetworkSummary.svelte
+++ src/lib/components/NetworkSummary.svelte
@@ -6,8 +6,15 @@
 	import type { OrcfaxStats } from '$lib/types';
 	import InfoBox from '$lib/components/InfoBox.svelte';
 	import { Nfc } from 'lucide-svelte';
+	import NodeIcon from '$lib/icons/NodeIcon.svelte';
+	import PingStatus from '$lib/components/PingStatus.svelte';
 
 	export let summary: OrcfaxStats;
+	$: totalNodes = summary.nodes.length;
+	$: activeNodes = summary.nodes.filter((node) => node.status === 'active').length;
+	$: totalSources = summary.sources.length;
+	$: dexSources = summary.sources.filter((source) => source.type === 'DEX LP').length;
+	$: cexSources = summary.sources.filter((source) => source.type === 'CEX API').length;
 </script>
 
 <section class="flex flex-col items-center">
@@ -18,7 +25,7 @@
 				<li>
 					<Card.Root class="h-full">
 						<Card.Header
-							class="flex flex-row items-center justify-between space-y-0 space-x-2 pb-2"
+							class="flex flex-row items-center justify-between space-y-0 space-x-2 pb-0"
 						>
 							<Card.Title class="text-sm font-medium">Active Feeds</Card.Title>
 							<Nfc class="stroke-primary" size="30" />
@@ -36,7 +43,7 @@
 				<li>
 					<Card.Root class="h-full">
 						<Card.Header
-							class="flex flex-row items-center justify-between space-y-0 space-x-6 pb-2"
+							class="flex flex-row items-center justify-between space-y-0 space-x-6 pb-0"
 						>
 							<Card.Title class="text-sm font-medium">Published Facts</Card.Title>
 							<FactIcon />
@@ -55,43 +62,47 @@
 				</li>
 				<li>
 					<Card.Root class="h-full">
-						<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+						<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-0">
 							<Card.Title class="text-sm font-medium">Data Sources</Card.Title>
 							<DataSourceIcon />
 						</Card.Header>
 						<Card.Content>
-							<div class="text-2xl font-bold">14</div>
-							<p class="text-xs mt-1">6 DEX LPs</p>
-							<p class="text-xs">8 CEX APIs</p>
+							<div class="text-2xl font-bold">{totalSources}</div>
+							<p class="text-xs mt-1">{dexSources} DEX LPs</p>
+							<p class="text-xs">{cexSources} CEX APIs</p>
 						</Card.Content>
 					</Card.Root>
 				</li>
 			</ul>
-			<InfoBox title="Orcfax v1 is live on Mainnet!">
-				<p>
-					Check out our <a
-						href="https://docs.orcfax.io"
-						target="_blank"
-						class="text-primary underline">docs</a
-					> to see what's new!
-				</p>
-			</InfoBox>
-		</div>
-
-		<!-- Loading Skeleton
-			<ul class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-				{#each Array(3) as _}
-					<li class="relative h-32 w-48 border bg-transparent rounded-lg">
-						<div class="flex flex-col w-full p-4 absolute top-0 left-0 space-y-2">
-							<div class="flex w-full justify-between">
-								<Skeleton class="h-5 w-20" />
-								<Skeleton class="h-6 w-6 rounded-full" />
-							</div>
-							<Skeleton class="h-8 w-12" />
-							<Skeleton class="h-4 w-20" />
+			<div class="grid gap-7 max-h-fit grid-cols-3">
+				<InfoBox title="Orcfax v1 is live on Mainnet!" className="col-span-2 h-fit self-center p-8">
+					<p>
+						Check out our <a
+							href="https://docs.orcfax.io"
+							target="_blank"
+							class="text-primary underline">docs</a
+						> to see what's new!
+					</p>
+				</InfoBox>
+				<Card.Root class="h-full col-span-1">
+					<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-0">
+						<Card.Title class="text-sm font-medium">Oracle Nodes</Card.Title>
+						<NodeIcon />
+					</Card.Header>
+					<Card.Content>
+						<div class="text-2xl font-bold">{totalNodes}</div>
+						<div class="flex items-center gap-2 self-end my-1">
+							<PingStatus
+								color={totalNodes === activeNodes ? 'green' : totalNodes >= 3 ? 'yellow' : 'red'}
+								size="md"
+							/>
+							<p class="text-xs">
+								{`${activeNodes} / ${totalNodes} Active`}
+							</p>
 						</div>
-					</li>
-				{/each}
-			</ul> -->
+					</Card.Content>
+				</Card.Root>
+			</div>
+		</div>
 	</div>
 </section>
